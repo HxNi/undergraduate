5ee92f320ca1f07ff7d5608e4c6d90d576b31fed
HxNi <HxNiHxNi@gmail.com>
2022-03-17 20:31:28 +0900

    v0.3 comment and rearrange of functions

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
+// [example]
+// pack example
 //wasmpack('wa/add.wasm', 0xBD);
 //wasmpack('wa/fac.wasm', 0xAA);
+
+// unpack example
 let main_wasm = wasmunpack('wa/add.wasp', 0xBD);
 wasmexec(main_wasm, aa);
 wasmexec(main_wasm, a);
@@ -18,15 +22,17 @@ function b(obj){
     console.log(obj.fac(2)); 
 }
 
-// inside
-
+// [pack/unpack API]
+// pack wrapper
 function wasmpack(filename, keyvalue) {
     fetch(filename).then(res => {
         let promise = res.body.getReader().read();
-        promise.then(res => pack(res, keyvalue)).then(save);
+        promise.then(res => pack(res, keyvalue))
+        .then(save); // (optional) for saving packed file
     });
 }
 
+// unpack wrapper
 async function wasmunpack(filename, keyvalue) {
     let res = await fetch(filename); // fetch(filename) -> [Response Promise] -> await -> res
     let res2 = await res.body.getReader().read(); // [Promise] res.body.getReader().read() -> await -> res2
@@ -40,6 +46,7 @@ async function wasmunpack(filename, keyvalue) {
     // });
 }
 
+// pack
 function pack(s, k) {
     let c = s.value;
     for (let i = 0; i < c.length; i++) {
@@ -48,14 +55,7 @@ function pack(s, k) {
     return s;
 }
 
-function save(c) {
-    let file = new Blob([c.value], {type: 'application/wasm'});
-    var url = URL.createObjectURL(file);
-    var link = document.getElementById('link');
-    link.href = url;
-    link.download = 'main.wasp';
-}
-
+// unpack
 function unpack(s, k) {
     let c = s.value;
     for (let i = 0; i < c.length; i++) {
@@ -64,12 +64,24 @@ function unpack(s, k) {
     return s;
 }
 
+// [subAPI]
+// make a link to download a wasm file in web
+function save(c) {
+    let file = new Blob([c.value], {type: 'application/wasm'});
+    var url = URL.createObjectURL(file);
+    var link = document.getElementById('link');
+    link.href = url;
+    link.download = 'main.wasp';
+}
+
+// instantiate a wasm file
 function instantiate(s) {
     let r = new Blob([s.value], {type: 'application/wasm'});
     let moduleResponse = new Response(r, {'headers': {'Content-Type': 'application/wasm'}});
     return WebAssembly.instantiateStreaming(moduleResponse);
 }
 
+// run a wasm file with given function name f
 function wasmexec(s, f) {
     s.then(obj => f(obj.instance.exports));
 }
